6ee7f5d6ced57caadc516c782fb7c98d81463783
Jinwoo Yu <jinyu385@student.liu.se>
2018-07-12 20:28:59 +0200

    Created a component for the navbar which collapses on small screens/windows. Using bootstrap vue.

---
+++ project/src/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<b-navbar toggleable="md" type="dark" variant="info">
+
+		  <b-navbar-brand href="#">
+		  	<img src="./assets/logo.png" alt="Logo" style="width:40px;">
+		  </b-navbar-brand>
+
+		  <!-- Collaps toggler -->
+		  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+		  <b-collapse is-nav id="nav_collapse"> 	
+
+		    <!-- Right aligned nav items -->
+		    <b-navbar-nav class="ml-auto">
+		    
+		    <!-- The links -->
+		    <b-navbar-nav>
+		      <b-nav-item href="#">Link1</b-nav-item>
+		      <b-nav-item href="#" disabled>Link2</b-nav-item>
+		    </b-navbar-nav>
+		    </b-navbar-nav>
+		  </b-collapse>
+
+		</b-navbar>
+	</div>
+</template>
--- project/src/main.js
+++ project/src/main.js
@@ -1,5 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+// Custom components.
+import Navbar from './Navbar.vue';
+
+// Import Bootstrap 4.
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+Vue.use(BootstrapVue)
+
+// Register custom components.
+Vue.component("app-navbar", Navbar);
 
 new Vue({
   el: '#app',
